815807eab088370d4ab768ef7f0fa420bf4885a3
Miller Alvarez <damialbla15@gmail.com>
2024-10-15 08:24:14 -0500

    Mejoras en el despliegue

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,28 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    sourcemap: false,
+    minify: 'esbuild',
+    target: 'esnext',
+    cssCodeSplit: true,
+    brotliSize: true,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+      },
+    },
+  },
+  css: {
+    devSourcemap: false,
+  },
+  optimizeDeps: {
+    include: ['axios', 'react', 'react-dom'],
+  }
 })
